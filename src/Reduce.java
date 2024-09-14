2c34d6a34f726638dbd6c46f61669aaf910f9e7b
Christine <christinecheng1229@gmail.com>
2024-09-14 13:24:56 -0400

    task 5 complete

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int steps = 0;
+        int value = 100;
+//        System.out.println("value:" + value);
+//        System.out.println("step" + steps);
+        while (value > 0) {
+            if (value % 2 == 0) {
+                value = value / 2;
+            } else --value;
+            ++steps;
+//            System.out.println("value:" + value);
+//            System.out.println("step" + steps);
+        }
+        System.out.println(steps);
+    }
 }
