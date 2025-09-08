9e3d3bac064739f6acd0d4027e20822e1b601e11
Christine <christinecheng1229@gmail.com>
2025-09-08 18:50:47 -0400

    task 3.2 solution

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(5);
         // FizzBuzz.main(); // causes an error since the main method expects an array of Strings argument
+            /*
+            Can be solved in 2 ways:
+            1) create a dummy argument:
+                String[] s = String[]{}
+            2) pass in the arg from this file into the Main method from the FizzBuzz file:
+                FizzBuzz.main(args)
+             */
     }
 }
